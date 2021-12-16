b46d74b77b7d18a45b91fd779168a1e0ddca70c1
Auddn <auddn33354@gmail.com>
2021-12-16 10:23:03 +0900

    Create indexVO.java

---
+++ src/main/java/itf/log4shell/log4shellVulnapp/model/indexVO.java
@@ -0,0 +1,13 @@
+package itf.log4shell.log4shellVulnapp.model;
+
+public class indexVO {
+    private String Payload;
+
+    public void setPayload(String payload) {
+        Payload = payload;
+    }
+
+    public String getPayload() {
+        return Payload;
+    }
+}
